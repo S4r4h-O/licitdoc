9acb84f4435f8867b982ac4f40e45ea10250659b
Sarah O. <sarah.shofani@proton.me>
2025-12-06 16:39:23 -0300

    fix: loading wrap

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,5 +1,7 @@
 export default function Loading() {
   return (
-    <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
+    <div className="flex items-center justify-center h-screen">
+      <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
+    </div>
   );
 }
