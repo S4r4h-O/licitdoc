45e38358f03388d70ce7ad618f986ffcad3d963d
Sarah O. <sarah.shofani@proton.me>
2025-12-06 18:52:49 -0300

    chore: add comment

--- lib/actions/s3.actions.ts
+++ lib/actions/s3.actions.ts
@@ -8,6 +8,11 @@ import { s3Client } from "../s3";
 export async function uploadFileToS3(base64: string, fileName: string) {
   const { userId: clerkUserID, orgId: clerkOrgId } = await auth();
 
+  /*
+   * Since server actions only allow serializable data,
+   * in the client component we convert the file to
+   * base64 and read it here
+   */
   const buffer = Buffer.from(base64, "base64");
   const bucketName = process.env.S3_BUCKET;
   const key = `${clerkOrgId}/${Date.now()}-${fileName}`;
