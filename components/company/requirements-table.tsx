690d5494ed9d9d62384372aeeb085e541e6ddb92
Sarah O. <sarah.shofani@proton.me>
2025-12-06 16:38:53 -0300

    feat: requirements table

---
+++ components/company/requirements-table.tsx
@@ -0,0 +1,221 @@
+"use client";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from "@tanstack/react-table";
+import { ArrowUpDown, Eye, MoreHorizontal, Pencil, Trash } from "lucide-react";
+import * as React from "react";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Input } from "../ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+
+import { DocumentRequirement } from "@prisma/client";
+import Link from "next/link";
+
+export default function DocumentRequirementTable({
+  data,
+}: {
+  data: DocumentRequirement[];
+}) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+
+  const columns: ColumnDef<DocumentRequirement>[] = React.useMemo(
+    () => [
+      {
+        accessorKey: "name",
+        header: ({ column }) => {
+          return (
+            <Button
+              variant="ghost"
+              onClick={() =>
+                column.toggleSorting(column.getIsSorted() === "asc")
+              }
+            >
+              Nome <ArrowUpDown />
+            </Button>
+          );
+        },
+        cell: ({ row }) => <div className="">{row.getValue("name")}</div>,
+      },
+      {
+        accessorKey: "jurisdictionLevel",
+        header: ({ column }) => {
+          return (
+            <Button
+              variant="ghost"
+              onClick={() =>
+                column.toggleSorting(column.getIsSorted() === "asc")
+              }
+            >
+              Jurisdição <ArrowUpDown />
+            </Button>
+          );
+        },
+      },
+      {
+        id: "actions",
+        enableHiding: false,
+        cell: ({ row }) => (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Abrir menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              align="end"
+              className="w-56 bg-neutral-600 space-y-4 p-2 rounded-md"
+            >
+              <DropdownMenuLabel>Ações</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="cursor-pointer focus:bg-accent rounded-md"
+                onSelect={(e) => e.preventDefault()}
+              >
+                <Link
+                  href={`/empresa/requisitos/${row.original.id}`}
+                  className="flex items-center w-full"
+                >
+                  <Eye className="mr-2 h-4 w-4" /> Ver
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex cursor-pointer text-red-600 focus:text-red-600 focus:bg-red-50 rounded-md"
+                onSelect={(e) => e.preventDefault()}
+              >
+                <Trash className="mr-2 h-4 w-4" /> Apagar
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ),
+      },
+    ],
+    [],
+  );
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filtrar Jurisdição"
+          value={
+            (table
+              .getColumn("jurisdictionLevel")
+              ?.getFilterValue() as string) ?? ""
+          }
+          onChange={(e) =>
+            table.getColumn("jurisdictionLevel")?.setFilterValue(e.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGourp) => (
+              <TableRow key={headerGourp.id}>
+                {headerGourp.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Sem resultados
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="text-muted-foreground flex-1 text-sm">
+          Mostrando {table.getRowModel().rows.length} de{" "}
+          {table.getFilteredRowModel().rows.length} registros
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Anterior
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Próximo
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
