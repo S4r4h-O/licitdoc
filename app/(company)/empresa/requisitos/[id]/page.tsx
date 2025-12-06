7d017237c60d7af1e21bce6bdb0e12c49e3720aa
Sarah O. <sarah.shofani@proton.me>
2025-12-06 18:53:53 -0300

    chore: remove HTML tags redundancy

--- app/(company)/empresa/requisitos/[id]/page.tsx
+++ app/(company)/empresa/requisitos/[id]/page.tsx
@@ -1,7 +1,8 @@
 import DocumentFileForm from "@/components/company/requirement-file-form";
+import RequirementFilesTable from "@/components/company/requirement-files-table";
 import UpdateDocumentRequirementForm from "@/components/company/update-requirement-form";
-import UploadFile from "@/components/upload-file";
 import { getDocumentRequirementById } from "@/lib/actions/doc-requirement.actions";
+import { getFilesByRequirementId } from "@/lib/actions/document-file.actions";
 
 export default async function RequirementDetailsPage({
   params,
@@ -11,15 +12,17 @@ export default async function RequirementDetailsPage({
   const { id } = await params;
 
   const docRequirement = await getDocumentRequirementById(id);
-  console.log(docRequirement);
+  const requirementFiles = await getFilesByRequirementId(docRequirement.id);
 
   return (
     <div>
-      <div>
-        <UpdateDocumentRequirementForm docRequirement={docRequirement} />
-      </div>
-      <div className="">
-        <DocumentFileForm requirement={docRequirement} />
+      <UpdateDocumentRequirementForm docRequirement={docRequirement} />
+      <hr />
+      <DocumentFileForm requirement={docRequirement} />
+      <hr />
+      <div className="p-6">
+        <h1 className="font-bold text-2xl">Documentos enviados</h1>
+        <RequirementFilesTable data={requirementFiles} />
       </div>
     </div>
   );
