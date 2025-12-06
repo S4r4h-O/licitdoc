e3771f9e15e08718be91ed1622d33d97e493c335
Sarah O. <sarah.shofani@proton.me>
2025-12-06 18:53:09 -0300

    feat: get files by requirement

--- lib/actions/document-file.actions.ts
+++ lib/actions/document-file.actions.ts
@@ -1,20 +1,21 @@
 "use server";
 
+import { prisma } from "@/prisma/client";
+import { auth } from "@clerk/nextjs/server";
+import { DocumentFile, DocumentRequirement } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 import { z } from "zod";
+
+import { formatError } from "../utils";
 import {
   DocumentFileFormSchema,
   DocumentFileInsertSchema,
 } from "../validators";
-import { auth } from "@clerk/nextjs/server";
-import { prisma } from "@/prisma/client";
-import { DocumentRequirement } from "@prisma/client";
-import { formatError } from "../utils";
-import { revalidatePath } from "next/cache";
 
 export async function createDocumentFile(
   data: z.infer<typeof DocumentFileFormSchema>,
   requirement: DocumentRequirement,
-) {
+): Promise<{ success: boolean; message: any }> {
   const { orgId: clerkOrgId, userId: clerkUserId } = await auth();
 
   if (!clerkOrgId) {
@@ -41,4 +42,32 @@ export async function createDocumentFile(
     console.error(error);
     return { success: false, message: formatError(error) };
   }
+}
+
+export async function getFilesByRequirementId(
+  reqId: string,
+): Promise<DocumentFile[] | []> {
+  const { orgId: clerkOrgId, userId: clerkUserId } = await auth();
+
+  if (!clerkOrgId) {
+    throw new Error("Organização não encontrada");
+  }
+
+  if (!clerkUserId) {
+    throw new Error("Não autenticado");
+  }
+
+  const requirement = await prisma.documentRequirement.findUnique({
+    where: { id: reqId, company: { clerkOrgId } },
+  });
+
+  if (!requirement) {
+    throw new Error("Requisito não encontrado");
+  }
+
+  const requirementFiles = await prisma.documentFile.findMany({
+    where: { requirement },
+  });
+
+  return requirementFiles;
 }
