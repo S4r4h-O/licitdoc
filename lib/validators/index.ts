f581a9fe39e831889c7be3fd309c345390dd9cea
Sarah O. <sarah.shofani@proton.me>
2025-12-07 11:27:37 -0300

    chore: add export

--- lib/validators/index.ts
+++ lib/validators/index.ts
@@ -3,3 +3,4 @@ export * from "./create-company.validator";
 export * from "./contracting-authority.validator";
 export * from "./doc-requirement.validator";
 export * from "./doc-requirement-file.validator";
+export * from "./licitacao.validators";
